db8a47d20296d0280e6265edfe9a83b75078bc68
vlada <aleks_ada@mai.ru>
2022-04-25 00:51:30 +0300

    Added task16

---
+++ src/javaLab/Task16.java
@@ -0,0 +1,36 @@
+package javaLab;
+
+//"Средний по больнице
+//        Разработать функцию, в которую передается в качестве аргументов массив типа float.
+//        Функция должна возвращать среднее арифметическое элементов массива.
+//        Если размер массива - нуль, то среднее арифметическое должно быть также нуль."
+
+
+public class Task16 {
+    float [] array = new float[5];
+
+    public static void main(String[] args) {
+        float [] array = new float[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] =(float) Math.random();
+            System.out.println(array[i]);
+
+        }
+        System.out.println(averageValue(array));
+    }
+
+    public static float averageValue(float[] array){
+        float result;
+        if(array.length == 0){
+            result = 0;
+        }else{
+            float sum = 0;
+            for (int i = 0; i < array.length; i++) {
+                sum += array[i];
+            }
+            result = sum/2;
+        }
+        return result;
+    }
+    
+}
